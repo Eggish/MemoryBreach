f94d78d1a9aabadcefd489f7d2e1d3f81ad270b9
Lövlie <fredriklovlie@gmail.com>
2020-01-22 17:03:23 +0100

    Project Initial Push

---
+++ Assets/PositionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] PlayerPositionMarkers;
+    private int Selector;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void MoveSelector()
+    {
+
+    } 
+}

3da2d15e592c6221c81300bd8a5ef8184135bccb
Lövlie <fredriklovlie@gmail.com>
2020-01-23 11:59:26 +0100

    Card Setup

---
+++ Assets/Card.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer[] Symbols = null;
+
+    private bool[] IsSymbolActive;
+
+    private void Awake()
+    {
+        foreach (SpriteRenderer s in Symbols)
+        {
+            s.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Setup(bool[] pIsSymbolActive)
+    {
+        IsSymbolActive = pIsSymbolActive;
+    }
+
+    public void TurnCard()
+    {
+        for(int i = 0; i < IsSymbolActive.Length; i++)
+        {
+            Symbols[i].enabled = IsSymbolActive[i];
+        }
+    }
+}
---
+++ Assets/CardManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    private Card[] Cards = null;
+
+    void Start()
+    {
+        Cards = GetComponentsInChildren<Card>();
+        foreach(Card c in Cards)
+        {
+            ActivateRandomSymbols(c);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void ActivateRandomSymbols(Card pCard)
+    {
+        bool[] isSymbolActive = 
+        { 
+            false,
+            false,
+            false
+        };
+        for(int i = 0; i < 3; i++)
+        {
+            int randomComparison = Random.Range(0, 2);
+            if(randomComparison == 1)
+            {
+                isSymbolActive[i] = true;
+            }
+        }
+
+        pCard.Setup(isSymbolActive);
+
+        pCard.TurnCard();
+    }
+}
